25c357a9bcee57562b53dc889fe8500ab52acadd
欧杰 <ouis123@163.com>
2020-03-19 16:32:30 +0800

    必须使用这个分支 ITMS-90809: Deprecated API Usage - Apple will stop accepting submissions of app updates that use UIWebView APIs starting from December 2020.

--- BlocksKit/UIKit/UIImagePickerController+BlocksKit.m
+++ BlocksKit/UIKit/UIImagePickerController+BlocksKit.m
@@ -45,10 +45,10 @@
 + (void)load
 {
 	@autoreleasepool {
-		[self bk_registerDynamicDelegate];
-		[self bk_linkDelegateMethods:@{ @"bk_didFinishPickingMediaBlock": @"imagePickerController:didFinishPickingMediaWithInfo:",
-                                        @"bk_didCancelBlock": @"imagePickerControllerDidCancel:" }];
-	}
+//		[self bk_registerDynamicDelegate];
+//		[self bk_linkDelegateMethods:@{ @"bk_didFinishPickingMediaBlock": @"imagePickerController:didFinishPickingMediaWithInfo:",
+//                                        @"bk_didCancelBlock": @"imagePickerControllerDidCancel:" }];
+//	}
 }
 
 @end
